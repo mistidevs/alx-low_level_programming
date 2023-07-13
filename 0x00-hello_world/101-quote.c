ceb67a171b375680a53ba43852834dfc8225c173
mistidevs-alx <mistidevs@gmail.com>
2023-07-13 12:19:14 -0700

    Betty coding corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 /**
-* main- print and that piece of art is useful" - Dora Korpar, 2015-10-19 followed by newline
+* main- print and that piece of art is useful" - Dora Korpar,
+* 2015-10-19 followed by newline
 * uses write and STDOUT_FILENO
 *
 * Return: 1 intentionally
@@ -10,8 +11,9 @@
 
 int main(void)
 {
-char my_string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write (STDOUT_FILENO, my_string, strlen(my_string));
+char my_string[] =
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDOUT_FILENO, my_string, strlen(my_string));
 
 return (1);
 }
