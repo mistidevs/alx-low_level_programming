b92332a4b1edb48106f35d24a02eaf44f36ffc92
mistidevs-alx <mistidevs@gmail.com>
2023-07-13 10:42:02 -0700

    C++ comments not allowed fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) //Prints string using function puts
+int main(void) /* Prints string using function puts */
 {
 char my_string[] = "\"Programming is like building a multilingual puzzle";
 puts(my_string);
