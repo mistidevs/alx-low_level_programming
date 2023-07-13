55e5c553dccb65806b0e12f3332b79b68eb07a2d
mistidevs-alx <mistidevs@gmail.com>
2023-07-13 11:00:01 -0700

    Adding newline

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,12 +4,12 @@
 * main- Prints with proper grammar, but the outcome is a piece of art,
 * uses- prinf function
 *
-* Return 0 if successful
+* Return: 0 if successful
 */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 }
