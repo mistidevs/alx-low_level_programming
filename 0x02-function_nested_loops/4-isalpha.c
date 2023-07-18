85073f336de67b4bf3f29af00e1de44cf6f04b80
mistidevs-alx <mistidevs@gmail.com>
2023-07-18 09:51:50 -0700

    Checking if input is a letter

---
+++ 0x02-function_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _isalpha - checking whether input is a letter using int c
+*
+* Return: 0 if successful
+*/
+
+int _isalpha(int c)
+{
+if ((c <= 90 && c >= 65) || (c <= 122 && c >= 97))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+return (0);
+}
