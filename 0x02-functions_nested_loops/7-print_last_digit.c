db46356ae242280f1536563191e936189ff66ee9
mistidevs-alx <mistidevs@gmail.com>
2023-07-19 03:21:51 -0700

    Eureka. Combining _putchar and return.

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
 * print_last_digit - printing the last digit of a number
+* @n: Integer to be checked
 *
 * Return: the last digit
 */
@@ -10,6 +12,7 @@ int print_last_digit(int n)
 {
 int last_digit;
 last_digit = abs(n % 10);
+_putchar(last_digit + '0');
 
 return (last_digit);
 }
