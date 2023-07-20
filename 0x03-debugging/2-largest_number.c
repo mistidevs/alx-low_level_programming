668bc4dff01737da9ba80d4dd55aba71d8b1917c
mistidevs-alx <mistidevs@gmail.com>
2023-07-20 14:09:10 -0700

    Including negative numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,6 +15,25 @@ if (a == b && b == c)
 {
 largest = a;
 }
+else if (a < 0 && b < 0 && c < 0)
+{
+if (a <= b && b <= c)
+{
+largest = a;
+}
+else if (b <= a && a <= c)
+{
+largest = b;
+}
+else if (c <= a && a <= b)
+{
+largest = c;
+}
+else
+{
+largest = a;
+}
+}
 else if (a > b && a > c)
 {
 largest = a;
