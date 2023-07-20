cdc20c106cd87612b329371ca7a92a7e3da68f37
mistidevs-alx <mistidevs@gmail.com>
2023-07-20 10:41:51 -0700

    Printing 1 to 100 factoring in divisibility by 3, 5 and 15

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - print fizz if divisible by 3
+* print buzz if divisible by 5
+* print fizzbuzz if divisible by 15
+*
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+int n;
+for (n = 1 ; n <= 100 ; n++)
+{
+if (n % 15 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (n % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", n);
+}
+}
+
+return (0);
+}
