4e13717c9bddcb5cd6a596acade67b0269c4e1b6
mistidevs-alx <mistidevs@gmail.com>
2023-07-20 13:22:33 -0700

    Printing numbers function

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+* print_number - print number of any length
+* @n: digit
+*
+* Return: nothing it is a void
+*/
+
+void print_number(int n)
+{
+int divisor;
+if (n == 0)
+{
+_putchar('0' + n);
+}
+else if (n > 0)
+{
+divisor = 1;
+while (n / divisor >= 10)
+{
+divisor *= 10;
+}
+
+while (divisor > 0)
+{
+int digit = n / divisor;
+_putchar(digit + '0');
+n %= divisor;
+divisor /= 10;
+}
+}
+else if (n < 0)
+{
+_putchar('-');
+n = -n;
+divisor = 1;
+while (n / divisor >= 10)
+{
+divisor *= 10;
+}
+
+while (divisor > 0)
+{
+int digit = n / divisor;
+_putchar(digit + '0');
+n %= divisor;
+divisor /= 10;
+}
+}
+}
+
