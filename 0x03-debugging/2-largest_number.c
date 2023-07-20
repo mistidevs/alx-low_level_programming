f41efa7c2c9a684bb3c5a1d413e5c91bb670f8c2
mistidevs-alx <mistidevs@gmail.com>
2023-07-20 14:03:14 -0700

    Bug with equal numbers and negative numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,16 +11,19 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-
-if (a > b && a > c)
+if (a == b && b == c)
 {
 largest = a;
 }
-else if (b > a && b > c)
+else if (a > b && b > c)
+{
+largest = a;
+}
+else if (b > a && a > c)
 {
 largest = b;
 }
-else
+else if (c > a && a > b)
 {
 largest = c;
 }
