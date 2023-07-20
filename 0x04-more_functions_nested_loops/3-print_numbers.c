e55c7c2f803bb7a6f611c40dad9270339019a815
mistidevs-alx <mistidevs@gmail.com>
2023-07-20 06:09:35 -0700

    Printing numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,5 +3,16 @@
 /**
 * print_numbers - printing numbers from 0 to 9
 *
-* Return: appropriate value
+* Return: void
 */
+
+void print_numbers(void)
+{
+int a;
+
+for (a = 0 ; a < 10 ; a++)
+{
+_putchar('0' + a);
+}
+_putchar('\n');
+}
