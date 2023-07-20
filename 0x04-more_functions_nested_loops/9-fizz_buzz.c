c4b8ef41210a66d98251c5d99d857bc91e9087da
mistidevs-alx <mistidevs@gmail.com>
2023-07-20 13:10:22 -0700

    Fizz Buzzing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,11 @@ int main(void)
 int n;
 for (n = 1 ; n <= 100 ; n++)
 {
-if (n % 15 == 0)
+if (n % 15 != 0 && n % 5 != 0 && n % 3 != 0)
+{
+printf("%d ", n);
+}
+else if (n % 15 == 0)
 {
 printf("FizzBuzz ");
 }
@@ -25,13 +29,10 @@ else if (n % 3 == 0)
 {
 printf("Fizz ");
 }
-else
-{
-printf("%d ", n);
-}
 }
 printf("\n");
 
 return (0);
 }
+
 
