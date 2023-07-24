30d846396d47162d3b860ac91472862f06d34348
mistidevs-alx <mistidevs@gmail.com>
2023-07-24 04:52:05 -0700

    Chaniging value of variable to 402 using a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - changing the value of an int to 98
+* @n: pointer used to change value in memory
+*
+* Return: it is avoid
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
