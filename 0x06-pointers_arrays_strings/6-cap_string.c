5f033bec31c14576adfa0c34b7232af473ead962
mistidevs-alx <mistidevs@gmail.com>
2023-07-26 11:20:55 -0700

    Capitalizing all first letters of words in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* cap_string - Capitalizing each word after '\t'
+* @p: string to capitalize
+*
+* Return: pointer to string
+*/
+
+char *cap_string(char *p)
+{
+int n;
+int ascii;
+char upper;
+int i;
+
+for (i = 0; p[i] != '\0'; i++)
+{
+	if (p[i] == '\t')
+	{
+		p[i] = ' ';
+	}
+}
+
+for (n = 0 ; p[n] != '\0' ; n++)
+{
+	if ((p[n] == '\t' || p[n] == '\n' || p[n] == '.' || p[n] == '!'
+	|| p[n] == ' ' || p[n] == '(' || p[n] == ')' || p[n] == ',' ||
+	p[n] == ';' || p[n] == '?' || p[n] == '\"' || p[n] == '{' ||
+	p[n] == '}') && (p[n + 1] >= 'a' && p[n + 1] <= 'z'))
+	{
+		ascii = p[n + 1] - 32;
+		upper = (char)ascii;
+		p[n + 1] = upper;
+	}
+}
+
+return (p);
+}
