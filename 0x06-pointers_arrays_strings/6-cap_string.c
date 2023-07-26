4c4d251977cdf14a1c17edf354ecb3fd845dc917
mistidevs-alx <mistidevs@gmail.com>
2023-07-26 12:11:59 -0700

    Capitalizing first letter of the string if not capitalised

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,6 +15,12 @@ char upper;
 
 for (n = 0 ; p[n] != '\0' ; n++)
 {
+        if (n == 0 && p[n] >= 'a' && p[n] <= 'z')
+        {
+		ascii = p[n] - 32;
+		upper = (char)ascii;
+		p[n] = upper;
+        }
 	if ((p[n] == '\t' || p[n] == '\n' || p[n] == '.' || p[n] == '!'
 	|| p[n] == ' ' || p[n] == '(' || p[n] == ')' || p[n] == ',' ||
 	p[n] == ';' || p[n] == '?' || p[n] == '\"' || p[n] == '{' ||
