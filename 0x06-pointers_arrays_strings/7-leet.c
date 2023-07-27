8b032c658dbd2a7df5f2a8528616a46ba3a5211b
mistidevs-alx <mistidevs@gmail.com>
2023-07-27 06:26:00 -0700

    Simplifying 7-leet.c to only two loops and one if

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,40 +9,22 @@
 
 char *leet(char *str)
 {
-int i;
-i = 0;
+int i, j;
+char s1[] = "aAeEoOtTlL"
+char s2[] = "4433007711"
 
-while (str[i] != '\0' && (str[i] >= 'a' && str[i] <= 'z'))
+for (i = 0 ; str[i] != '\0' ; i++)
 {
-if (str[i] == 'a' || str[i] == 'A')
-{
-	str[i] = '4';
-}
-else if (str[i] == 'e' || str[i] == 'E')
-{
-	str[i] = '3';
-}
-else if (str[i] == 'o' || str[i] == 'O')
-{
-	str[i] = '0';
-}
-else if (str[i] == 't' || str[i] == 'T')
-{
-	str[i] = '7';
-}
-else if (str[i] == 'l' || str[i] == 'L')
-{
-	str[i] = '1';
-}
-else
-{
-	str[i] = str[i];
-}
-i++;
+	for (j = 0 ; j < 10 ; j++)
+	{
+		if (str[i] == s1[j])
+		{
+			str[i] = s2[j];
+		}
+	}
 }
 
-return (str);
+return (n);
 }
-
 
 
