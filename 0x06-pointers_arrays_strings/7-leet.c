9e8338a548a1d60bad5e5ed20411f6b9b7a4b7ea
mistidevs-alx <mistidevs@gmail.com>
2023-07-27 02:24:30 -0700

    Removing for ternary for 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* leet - changing letters from form to another
+* leet - str[i]hanging letters from form to another
 * @str: string to 1337
 *
 * Return: pointer to string
@@ -9,45 +9,38 @@
 
 char *leet(char *str)
 {
-int len;
 int i;
-len = 0;
+i = 0;
 
-while (str[len] != '\0')
+while (str[i] != '\0')
 {
-	len++;
+if (str[i] == 'a' || str[i] == 'A')
+{
+	str[i] = '4';
+}
+else if (str[i] == 'e' || str[i] == 'E')
+{
+	str[i] = '3';
+}
+else if (str[i] == 'o' || str[i] == 'O')
+{
+	str[i] = '0';
+}
+else if (str[i] == 't' || str[i] == 'T')
+{
+	str[i] = '7';
+}
+else if (str[i] == 'l' || str[i] == 'L')
+{
+	str[i] = '1';
+}
+else
+{
+	str[i] = str[i];
+}
+i++;
 }
 
-
-for (i = 0; i < len; i++)
-{
-	if (str[i] == 'a' || str[i] == 'A')
-	{
-		str[i] = '4';
-	}
-	else if (str[i] == 'e' || str[i] == 'E')
-	{
-		str[i] = '3';
-	}
-	else if (str[i] == 'o' || str[i] == 'O')
-	{
-		str[i] = '0';
-	}
-	else if (str[i] == 't' || str[i] == 'T')
-	{
-		str[i] = '7';
-	}
-	else if (str[i] == 'l' || str[i] == 'L')
-	{
-		str[i] = '1';
-	}
-	else
-	{
-		str[i] = str[i];
-	}
-}
-
-str[len] = '\0';
 return (str);
 }
 
