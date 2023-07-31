a8154bed74c79428c93bb73691c09a5704231c75
mistidevs-alx <mistidevs@gmail.com>
2023-07-31 04:25:32 -0700

    Copy memory area

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ int _putchar(char c);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
