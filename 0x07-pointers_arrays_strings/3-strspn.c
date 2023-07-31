71342f515622318a56621b99cb5858142ef8a71b
mistidevs-alx <mistidevs@gmail.com>
2023-07-31 09:25:34 -0700

    Getting length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+* _strlen - gets length of string
+* @p: string to get length of
+*
+* Return: length of string
+*/
+
+unsigned int _strlen(char *p)
+{
+int x;
+
+for (x = 0; p[x] != '\0'; x++)
+{
+        x++;
+}
+
+return (x);
+}
+
+/**
+* _strspn - getting number of bytes only in
+* another array
+* @s: array to search
+* @accept: array used to search
+*
+* Return: number of bytes
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j, p;
+int flag;
+p = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	flag = 0;
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+		if (s[i] == accept[j])
+		{
+			p++;
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0)
+	{
+		break;
+	}
+}
+
+return (p);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
