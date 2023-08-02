abd6000328a78354d41bca95d551fd9ef03abccc
mistidevs-alx <mistidevs@gmail.com>
2023-08-02 05:00:21 -0700

    Adding documentation to the code

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+* is_prime_helper - adding var that aids in operation
+* @n: number to check if prime
+* @i: recursively incremented divisor
+*
+* Return: 1 or 0 if number is prime
+*/
+
 int is_prime_helper(int n, int i)
 {
 if (i == n)
@@ -17,6 +25,13 @@ else
 	return (is_prime_helper(n, i + 1));
 }
 }
+
+/**
+* is_prime_number - checking if a number is prime recursively
+* @n: number to check if prime
+*
+* Return: 1 or 0 if number is prime
+*/
 
 int is_prime_number(int n)
 {
