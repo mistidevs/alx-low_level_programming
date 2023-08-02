533190eb7d647d24fb31eb0500cf7742d6072824
mistidevs-alx <mistidevs@gmail.com>
2023-08-02 05:28:21 -0700

    Adding documentation to 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,32 @@
 #include "main.h"
 
+/**
+* palindrome_helper - adding paramaters to check string
+* @s: string being checked
+* @left: value of left digit being checked
+* @right: value of right digit being checked
+*
+* Return: 1 or 0 depending on whether string is palindrome
+*/
+
 int palindrome_helper(char *s, int left, int right)
 {
 if (left >= right)
 	return (1);
 
 if (s[left] != s[right])
-	return(0);
+	return (0);
 
 else
 	return (palindrome_helper(s, left + 1, right - 1));
 }
+
+/**
+* is_palindrome - checking if a string is a palindrome
+* @s: string being checked
+*
+* Return: 1 or 0 depending on whether string is a palindrome
+*/
 
 int is_palindrome(char *s)
 {
