c50cb889e87874aac807323a0409cb3fad51d5fc
mistidevs-alx <mistidevs@gmail.com>
2023-08-06 06:08:25 -0700

    Using strtol rather than atoi to deal with more digits for 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,20 +14,18 @@
 int main(int argc, char *argv[])
 {
 int i, sum;
+char *endptr;
 sum = 0;
 
 if (argc > 1)
 {
 	for (i = 1; i < argc; i++)
 	{
-		if (isdigit(argv[i][0]) && argv[i][0] >= '0' && argv[i][0] <= '9')
-		{
-			sum += atoi(argv[i]);
-		}
-		else
+		sum += strtol(argv[i], &endptr, 10);
+		if (*endptr != '\0')
 		{
 			printf("Error\n");
-                        return (1);
+			return (1);
 		}
 
 	}
