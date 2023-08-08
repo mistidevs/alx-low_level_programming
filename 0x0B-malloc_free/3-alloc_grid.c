998ccf35099f98510096c3ec6366c684d7cd8243
mistidevs-alx <mistidevs@gmail.com>
2023-08-08 06:44:40 -0700

    Creating a grid and initializing to zero(memory leak present)

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* alloc_grid - a pointer to an integral array initialized to 0
+* @width: width of the array
+* @height: height of the array
+*
+* Return: pointer to array
+*/
+
+int **alloc_grid(int width, int height)
+{
+int i, j;
+int **arr = malloc(sizeof(int *) * height);
+
+if (height == 0 || width == 0)
+{
+	return (NULL);
+}
+
+for (i = 0; i < height; i++)
+{
+	arr[i] = malloc(sizeof(int) * width);
+	if (arr[i] == NULL)
+	{
+		for (j = 0; j < i; j++)
+		{
+			free(arr[j]);
+		}
+		free (arr);
+		return (NULL);
+	}
+}
+
+for (i = 0; i < height; i++)
+{
+	for (j= 0; j < width; j++)
+	{
+		arr[i][j] = 0;
+	}
+}
+
+return (arr);
+}
+
