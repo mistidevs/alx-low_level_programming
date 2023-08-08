b02c27610eeb8a34e83e18710ef77b66df65b2b1
mistidevs-alx <mistidevs@gmail.com>
2023-08-08 04:40:36 -0700

    Allocating memory with malloc and storing a copy of malloc in a pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _strdup - copying a sttring and returning a pointer to the duplicate
+* @str: sttring to duplicate
+*
+* Return; pointer to duplicate or NULL
+*/
+
+char *_strdup(char *str)
+{
+char *st;
+int i;
+int len;
+len = 0;
+i = 0;
+
+if (str == NULL)
+{
+	return (NULL);
+}
+
+while (str[len] != '\0')
+{
+	len++;
+}
+
+st = malloc(sizeof(char) * len);
+
+if (st == NULL)
+{
+	return (NULL);
+}
+
+while (i < len)
+{
+	st[i] = str[i];
+	i++;
+}
+
+st[i] = '\0';
+
+return (st);
+}
