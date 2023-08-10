f21246bc4f587cc0a0be0a137d3d7ce4c0208eae
mistidevs-alx <mistidevs@gmail.com>
2023-08-10 04:23:35 -0700

    Code simplification using inbuilt C functions rather than iteration

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,29 +15,27 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
-unsigned int len, len1, len2, i, j;
 
 if (s1 == NULL)
 	s1 = "";
 if (s2 == NULL)
 	s2 = "";
 
-len1 = strlen(s1);
-len2 = strlen(s2);
-
-if (n >= len2)
+if (n >= strlen(s2))
 {
-	n = len2;
-	s = malloc(sizeof(char) * (len1 + n + 1));
-	if (s == NULL)
-		return (NULL);
+	n = strlen(s2);
 }
 
-for (i = 0; i < len1; i++)
-	s[i] = s1[i];
-for (j = 0; j < n; j++, i++)
-	s[i] = s2[j];
-s[i] = '\0';
+s = malloc(strlen(s1) + n + 1);
+
+if (s == NULL)
+	return (NULL);
+
+strcpy(s, s1);
+
+memcpy(s + strlen(s1), s2, n);
+
+s[strlen(s1) + n] = '\0';
 
 return (s);
 }
