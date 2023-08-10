7d3f625097b8e74be676e593b6aa2f80da7710cd
mistidevs-alx <mistidevs@gmail.com>
2023-08-10 03:21:36 -0700

    Handling edge case where n is greater than the length of s2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <string.h>
 
 /**
 * string_nconcat - concetenating two strings with n bytes of s2
@@ -16,19 +17,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *s;
 unsigned int len, len1, len2, i, j;
 len1 = len2 = 0;
-
 if (s1 != NULL)
-{
-	while (s1[len1] != '\0')
-		len1++;
-}
+	len1 = strlen(s1);
 else
 	len1 = 0;
 if (s2 != NULL)
-{
-	while (s2[len2] != '\0')
-		len2++;
-}
+	len2 = strlen(s2);
 else
 	len2 = 0;
 if (n >= len2)
@@ -45,12 +39,19 @@ else
 	if (s == NULL)
 		return (NULL);
 }
-
 for (i = 0; i < len1; i++)
 	s[i] = s1[i];
-
-for (j = 0; i < len; j++, i++)
-	s[i] = s2[j];
+if (n < len2)
+{
+	for (j = 0; j < n; j++)
+		s[i + j] = s2[j];
+	s[i + j] = '\0';
+}
+else
+{
+	for (j = 0; j < len2; j++)
+		s[i + j] = s2[j];
+}
 
 return (s);
 }
