89fc22407d109434b8c0159baccc64a304e3c499
mistidevs-alx <mistidevs@gmail.com>
2023-08-14 09:29:50 -0700

    Program that prints name of source code file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Printing the name of the file it is compiled from
+*
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+
+return (0);
+}
