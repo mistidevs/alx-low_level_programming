6c8c8db971369e4b17a9bb261afeeb02df83e8b7
mistidevs-alx <mistidevs@gmail.com>
2023-08-14 12:46:09 -0700

    Checking if name and owner have values first

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,9 +19,17 @@ new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 	return (NULL);
 
-new_dog->name = name;
+if (name == NULL)
+	return (NULL);
+else
+	new_dog->name = name;
+
 new_dog->age = age;
-new_dog->owner = owner;
+
+if (owner == NULL)
+	return (NULL);
+else
+	new_dog->owner = owner;
 
 return (new_dog);
 }
