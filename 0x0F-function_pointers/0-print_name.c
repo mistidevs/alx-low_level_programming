0974ba1ff9b1990ad28ba0d2100ddf100a403c0b
mistidevs-alx <mistidevs@gmail.com>
2023-08-16 04:47:17 -0700

    Error handling if pointer or function pointer are NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
 * print_name - printing name depending on function pointed to
@@ -10,5 +11,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+if (name == NULL || f == NULL)
+	return;
+
 f(name);
 }
