d37e6d46a567d0e8a0f9982c59d24098bff26aa4
mistidevs-alx <mistidevs@gmail.com>
2023-08-17 11:15:05 -0700

    Betty styling and code compression

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,40 +2,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
+
+/**
+* print_char - printing character c
+* @c: character to print
+*
+* Return: void
+*/
 
 void print_char(char c)
 {
 printf("%c", c);
 }
 
+/**
+* print_int - printing int i
+* @i: int to print
+*
+* Return: void
+*/
 
 void print_int(const unsigned int i)
 {
 printf("%d", i);
 }
 
+/**
+* print_float - printing double f
+* @f: double to print
+*
+* Return: void
+*/
+
 void print_float(double f)
 {
 printf("%f", f);
 }
 
-void print_string(const char *s)
-{
-if (s == NULL)
-{
-	printf("(nil)");
-	return;
-}
-
-printf("%s", s);
-}
+/**
+* print_all - printing any value of accepted type
+* @format: format of value to print
+* @...: parameters to print
+*
+* Return: void
+*/
 
 void print_all(const char * const format, ...)
 {
 int i, j;
 va_list ar;
-char arr[4] = {'c', 'i', 'f', 's'};
+char arr[4] = {'c', 'i', 'f', 's'}, *s;
 i = j = 0;
 va_start(ar, format);
 
@@ -53,7 +69,13 @@ while (format[i] != '\0')
 			print_float(va_arg(ar, double));
 			break;
 		case 's':
-			print_string(va_arg(ar, const char *));
+			s = va_arg(ar, char *);
+			if (s == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", s);
 			break;
 		default:
 			break;
@@ -64,10 +86,7 @@ while (format[i] != '\0')
 			printf(", ");
 		j++;
 	}
-	i++;
-	j = 0;
+	i++, j = 0;
 }
-
-printf("\n");
-va_end(ar);
+printf("\n"), va_end(ar);
 }
