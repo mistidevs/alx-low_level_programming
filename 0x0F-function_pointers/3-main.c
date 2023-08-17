9e65505fa3bf869dfb862be4be6105d8bf0ae7d1
mistidevs-alx <mistidevs@gmail.com>
2023-08-17 01:24:57 -0700

    Dealing with multibyte operands to yield error rather than having a segmentation fault

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,18 +13,18 @@
 
 int main(int argc, char **argv)
 {
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[3]);
-int result;
+int num1, num2, result;
 
-if (argc != 4)
+if (argc < 4)
 {
 	printf("Error\n");
 	exit(98);
 }
 
-if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
-argv[2][0] != '/' && argv[2][0] != '%')
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+
+if (argv[2][1] != '\0')
 {
 	printf("Error\n");
 	exit(99);
