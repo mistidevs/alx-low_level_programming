563c619618049bcf23f34c0c6f8a9a5c14e14cc0
mistidevs-alx <mistidevs@gmail.com>
2023-08-17 06:01:56 -0700

    Printing strings delimited by a string followed by a new using a variadic function

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* print_strings - printing strings followed by a new line
+* @separator: string separator for the strings
+* @n: number of parameters to print
+* @...: paramaters to print
+*
+* Return: void
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list am;
+unsigned int i;
+const char  *string;
+
+if (n <= 0)
+	return;
+
+va_start(am, n);
+
+for (i = 0; i < n; i++)
+{
+	string = va_arg(am, const char *);
+	if (string == NULL)
+		printf("(nil)");
+	else
+		printf("%s", string);
+	if (i != (n - 1) && separator != NULL)
+		printf("%s", separator);
+}
+
+printf("\n");
+
+va_end(am);
+}
