0f81e836548045738d21c1cfd21c95fb3fba7790
mistidevs-alx <mistidevs@gmail.com>
2023-08-20 03:55:26 -0700

    Dealing with print_all(NULL)

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,42 +4,6 @@
 #include <stdarg.h>
 
 /**
-* print_char - printing character c
-* @c: character to print
-*
-* Return: void
-*/
-
-void print_char(char c)
-{
-printf("%c", c);
-}
-
-/**
-* print_int - printing int i
-* @i: int to print
-*
-* Return: void
-*/
-
-void print_int(const unsigned int i)
-{
-printf("%d", i);
-}
-
-/**
-* print_float - printing double f
-* @f: double to print
-*
-* Return: void
-*/
-
-void print_float(double f)
-{
-printf("%f", f);
-}
-
-/**
 * print_all - printing any value of accepted type
 * @format: format of value to print
 * @...: parameters to print
@@ -51,41 +15,36 @@ void print_all(const char * const format, ...)
 {
 int i, j;
 va_list ar;
-char arr[4] = {'c', 'i', 'f', 's'}, *s;
+char *s, *str;
 i = j = 0;
 va_start(ar, format);
 if (format)
 {
-while (format[i] != '\0')
-{
-	switch (format[i])
+	while (format[i] != '\0')
 	{
-		case 'c':
-			print_char(va_arg(ar, int));
-			break;
-		case 'i':
-			print_int(va_arg(ar, int));
-			break;
-		case 'f':
-			print_float(va_arg(ar, double));
-			break;
-		case 's':
-			s = va_arg(ar, char *);
-			if (!s)
-				s = "(nil)";
-			printf("%s", s);
-			break;
-		default:
-			break;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", s, va_arg(ar, int));
+				break;
+			case 'i':
+				printf("%s%d", s, va_arg(ar, int));
+				break;
+			case 'f':
+				printf("%s%f", s, va_arg(ar, double));
+				break;
+			case 's':
+				str = va_arg(ar, char *);
+				if (!s)
+					str = "(nil)";
+				printf("%s%s", s, str);
+				break;
+			default:
+				break;
 		}
-	while (j < 4)
-	{
-		if (format[i] == arr[j] && format[i + 1] != '\0')
-			printf(", ");
-		j++;
+		s = ", ";
+		i++, j = 0;
 	}
-	i++, j = 0;
-}
 }
 printf("\n"), va_end(ar);
 }
