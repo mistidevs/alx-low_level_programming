d4b025637a81e134a5373ff3cb2e8ff4630f65d5
mistidevs-alx <mistidevs@gmail.com>
2023-08-24 12:50:17 -0700

    Using no intterupts at assembly level

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,5 @@
 section .data
-    format db "Hello, Holberton", 0
-    newline db 10, 0  ; Newline character and null terminator
+    format db "Hello, Holberton", 10, 0
 
 section .text
     global main
@@ -11,7 +10,5 @@ main:
     push rbp
     mov rdi, format  ; Set the format string argument for printf
     call printf      ; Call the printf function
-    mov rdi, newline ; Set the newline character argument for printf
-    call printf      ; Call the printf function again for the newline
     pop rbp
     ret
