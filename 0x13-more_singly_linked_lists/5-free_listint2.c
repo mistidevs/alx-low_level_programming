031966bec1f8cc12879cc5ac4612aa492114e2ec
mistidevs-alx <mistidevs@gmail.com>
2023-08-28 10:46:11 -0700

    Freeing a list and setting head to NULL algorithm

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* free_listint2 - free a list and set head to NULL
+* @head: pointer to first node of the list
+*
+* Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *current = *head;
+
+*head = NULL;
+
+while (current != NULL)
+{
+	listint_t *next = current->next;
+
+	free(current);
+	current = next;
+}
+}
