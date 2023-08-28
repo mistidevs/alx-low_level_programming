88e1d9566468302df3bb9cf90806bb01121dcb14
mistidevs-alx <mistidevs@gmail.com>
2023-08-28 09:57:07 -0700

    Printing elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* print_listint - print elements in a singly linked list
+* @h: pointer to listint_t singly linked list
+*
+* Return: number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+const listint_t *current;
+size_t count;
+
+current = h;
+count = 0;
+
+while (current != NULL)
+{
+	if (!(current->n))
+		printf("(nil)\n"), count++;
+	else
+		printf("%d\n", current->n), count++;
+	current = current->next;
+}
+
+return (count);
+}
