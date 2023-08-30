877e56f73edc205f916da61a0b7027c44a507138
mistidevs-alx <mistidevs@gmail.com>
2023-08-30 12:37:29 -0700

    Adding loop check to ensure printing of non-loop list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+* free_listint_safe - freeing a list with a loop
+* @h: pointer to pointer to the head of a list
+*
+* Return: size of list freed
+*/
+
+size_t free_listint_safe(listint_t **h)
+{
+size_t count;
+
+}
