df1ed66902bd9b9e57b89d8e900a609db3d59d3b
mistidevs-alx <mistidevs@gmail.com>
2023-08-30 12:42:59 -0700

    Reverting to previous state

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,32 +10,16 @@
 size_t print_listint_safe(const listint_t *head)
 {
 size_t count = 0;
-const listint_t *slow, *fast;
-int loop_found = 0;
 
 if (head == NULL)
 	exit(98);
-
-slow = fast = head;
-while (fast != NULL && fast->next != NULL)
-{
-	slow = slow->next;
-	fast = fast->next->next;
-	count++;
-
-	if (slow == fast)
-	{
-		loop_found = 1;
-		break;
-	}
-}
 
 while (head != NULL)
 {
 	printf("[%p] %d\n", (void *)head, head->n);
 	count++;
 
-	if ((void *)head <= (void *)(head->next) && loop_found == 1)
+	if ((void *)head <= (void *)(head->next))
 	{
 		printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 		break;
