73bbda70171416eae82cb33f10c24da743d05f0f
mistidevs-alx <mistidevs@gmail.com>
2023-09-03 06:04:05 -0700

    Execution improvement

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,9 @@
-section .data
-    format db "Hello, Holberton", 10, 0
-
-section .text
-    global main
-
-extern printf
-
+          global    main
+          extern    printf
 main:
-    push rbp
-    mov rdi, format  ; Set the format string argument for printf
-    call printf      ; Call the printf function
-    pop rbp
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
