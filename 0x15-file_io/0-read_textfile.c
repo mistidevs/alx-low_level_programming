12884fc7bfbf6542531c9dcb44761a5508a2c445
mistidevs-alx <mistidevs@gmail.com>
2023-09-04 01:18:25 -0700

    Reading a file and writing to STDOUT_FILENO

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* read_textfile - reading and writing text file to standard output
+* @filename: pointer to file to read
+* @letters: maximum number of letters to write
+*
+* Return: number of letters written
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t count;
+int fd, fr;
+char ch;
+
+count = 0;
+if (filename == NULL)
+	return (0);
+
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+	return (0);
+
+while (count < (ssize_t)letters && read(fd, &ch, 1) > 0)
+{
+	fr = write(STDOUT_FILENO, &ch, 1);
+	if (fr != 1)
+		return (0);
+	count++;
+}
+
+return (count);
+}
