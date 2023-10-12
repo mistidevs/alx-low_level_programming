db7ea8adb2ecb82f4062b04023f6cac8fe1d6408
mistidevs-alx <mistidevs@gmail.com>
2023-10-12 09:35:43 -0700

    Handling case where node is added at index zero in NULL and filled list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,43 +11,44 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *curr, *next, *new_node;
-unsigned int i;
-
-if (*h == NULL && idx != 0)
-	return (NULL);
-
-curr = *h;
-i = 0;
-while (curr != NULL && i < idx)
-{
-	curr = curr->next;
-	i++;
-}
-
-if (i != idx || curr == NULL)
-	return (NULL);
+dlistint_t *curr, *new_node;
+unsigned int i = 0;
 
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 	return (NULL);
 
 new_node->n = n;
-if (*h == NULL && idx == 0)
+new_node->prev = NULL;
+new_node->next = NULL;
+
+if (idx == 0)
 {
+	new_node->next = *h;
+	if (*h != NULL)
+		(*h)->prev = new_node;
 	*h = new_node;
-	new_node->prev = NULL;
-	new_node->next = NULL;
 	return (new_node);
 }
 
-curr = curr->prev;
-new_node->prev = curr;
+curr = *h;
+while (curr != NULL && i < idx - 1)
+{
+	curr = curr->next;
+	i++;
+}
+
+if (curr == NULL)
+{
+	free(new_node);
+	return (NULL);
+}
+
 new_node->next = curr->next;
+new_node->prev = curr;
+if (curr->next != NULL)
+	curr->next->prev = new_node;
 curr->next = new_node;
-next = curr->next;
-if (next != NULL)
-	next->prev = new_node;
 
 return (new_node);
 }
