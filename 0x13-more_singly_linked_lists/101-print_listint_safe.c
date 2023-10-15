79a65c46ae5a00ebc3c6f736efa84a0c1360db68
mistidevs <mistidevs@gmail.com>
2023-10-15 11:59:21 +0300

    Using a Hash Table to print a linked list with a loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,10 +10,18 @@
 size_t print_listint_safe(const listint_t *head)
 {
 size_t count = 0;
+hash_table_t *ht = NULL;
 
 if (head == NULL)
 {
 	printf("0\n");
+	exit(98);
+}
+
+ht = hash_table_create(1024);
+if (ht == NULL)
+{
+	printf("Error: could not create hash table\n");
 	exit(98);
 }
 
@@ -22,14 +30,16 @@ while (head != NULL)
 	printf("[%p] %d\n", (void *)head, head->n);
 	count++;
 
-	if ((void *)head <= (void *)(head->next))
+	if (hash_table_get(ht, (char *)head) != NULL)
 	{
 		printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-		break;
+    		break;
 	}
 
+	hash_table_set(ht, (char *)head, (char *)head);
 	head = head->next;
 }
 
+hash_table_delete(ht);
 return (count);
 }
