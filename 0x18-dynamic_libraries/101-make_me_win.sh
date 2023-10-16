70f7f42793cbd75b06a1a057389c7e51bdab3c50
mistidevs-alx <mistidevs@gmail.com>
2023-10-16 11:14:31 -0700

    Testing!!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/mistidevs/alx-low_level_programming/raw/master/0x18-dynamic_libraries/override.so
-export LD_PRELOAD=/tmp/override.so
+wget -q -O /tmp/winwin.so https://github.com/ermiasbahru/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winwin.so
+export LD_PRELOAD=/tmp/winwin.so
